bb0a51dd5c7922a1cb21aaa2b36afa411ed70905
Flávio Ferreira <flavinhoapf@hotmail.com>
2020-03-19 17:27:25 -0300

    Criadas as funções de Exclusão e Localizar.

--- ControleDeEstoque/DAL/DALCategoria.cs
+++ ControleDeEstoque/DAL/DALCategoria.cs
@@ -1,6 +1,7 @@
 ﻿using Modelo;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -39,6 +40,32 @@ namespace DAL
             conexao.Conectar();
             cmd.ExecuteNonQuery();
             conexao.Desconectar();
+        }
+
+        public void Excluir(int codigo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "delete from categoria where cat_cod = @codigo;";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Conectar();
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
+        }
+
+        public DataTable Localizar(String valor)
+        {
+            DataTable tabela = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("select * from categoria where cat_nome like '%" + valor + "%'", conexao.StringConexao);
+            da.Fill(tabela);
+            return tabela;
+        }
+
+        public ModeloCategoria CarregaModeloCategoria(int codigo)
+        {
+            ModeloCategoria modelo = new ModeloCategoria();
+            SqlCommand cmd = new SqlCommand();
+
         }
     }
 }
