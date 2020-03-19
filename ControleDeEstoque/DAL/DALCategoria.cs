124f249798c800506308ee70edaa1d2eee709de0
Flávio Ferreira <flavinhoapf@hotmail.com>
2020-03-19 14:50:57 -0300

    Foi criada a conexão com o banco de dados; Criamos os métodos de Incluir e Alterar Categorias.

---
+++ ControleDeEstoque/DAL/DALCategoria.cs
@@ -0,0 +1,44 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class DALCategoria
+    {
+        private DALConexao conexao;
+
+        public DALCategoria(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+
+        public void Incluir(ModeloCategoria modelo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "insert into categoria(cat_nome) values(@nome); select @@IDENTITY;";
+            cmd.Parameters.AddWithValue("@nome", modelo.Cat_nome);
+            conexao.Conectar();
+            modelo.Cat_cod = Convert.ToInt32(cmd.ExecuteScalar());
+            conexao.Desconectar();
+        }
+
+        public void Alterar(ModeloCategoria modelo)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "update categoria set cat_nome = @nome where cat_cod = @codigo;";
+            cmd.Parameters.AddWithValue("@nome", modelo.Cat_nome);
+            cmd.Parameters.AddWithValue("@codigo", modelo.Cat_cod);
+            conexao.Conectar();
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
+        }
+    }
+}
