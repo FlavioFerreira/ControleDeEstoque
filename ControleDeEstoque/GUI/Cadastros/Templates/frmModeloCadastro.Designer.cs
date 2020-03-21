cbd6e0325f10926ffacaf5c6350670293ac60f59
Flávio Ferreira <flavinhoapf@hotmail.com>
2020-03-21 12:24:50 -0300

    Começando a criar o modelo da tela de cadastros.

---
+++ ControleDeEstoque/GUI/Cadastros/Templates/frmModeloCadastro.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace GUI.Cadastros.Templates
+{
+    partial class frmModeloCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnDados = new System.Windows.Forms.Panel();
+            this.pnBotoes = new System.Windows.Forms.Panel();
+            this.btInserir = new System.Windows.Forms.Button();
+            this.btLocalizar = new System.Windows.Forms.Button();
+            this.btAlterar = new System.Windows.Forms.Button();
+            this.btExcluir = new System.Windows.Forms.Button();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.pnBotoes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnDados
+            // 
+            this.pnDados.Location = new System.Drawing.Point(1, 12);
+            this.pnDados.Name = "pnDados";
+            this.pnDados.Size = new System.Drawing.Size(771, 445);
+            this.pnDados.TabIndex = 0;
+            // 
+            // pnBotoes
+            // 
+            this.pnBotoes.Controls.Add(this.btCancelar);
+            this.pnBotoes.Controls.Add(this.btSalvar);
+            this.pnBotoes.Controls.Add(this.btExcluir);
+            this.pnBotoes.Controls.Add(this.btAlterar);
+            this.pnBotoes.Controls.Add(this.btLocalizar);
+            this.pnBotoes.Controls.Add(this.btInserir);
+            this.pnBotoes.Location = new System.Drawing.Point(1, 463);
+            this.pnBotoes.Name = "pnBotoes";
+            this.pnBotoes.Size = new System.Drawing.Size(771, 87);
+            this.pnBotoes.TabIndex = 1;
+            // 
+            // btInserir
+            // 
+            this.btInserir.Location = new System.Drawing.Point(28, 19);
+            this.btInserir.Name = "btInserir";
+            this.btInserir.Size = new System.Drawing.Size(75, 23);
+            this.btInserir.TabIndex = 0;
+            this.btInserir.Text = "Inserir";
+            this.btInserir.UseVisualStyleBackColor = true;
+            // 
+            // btLocalizar
+            // 
+            this.btLocalizar.Location = new System.Drawing.Point(122, 19);
+            this.btLocalizar.Name = "btLocalizar";
+            this.btLocalizar.Size = new System.Drawing.Size(75, 23);
+            this.btLocalizar.TabIndex = 1;
+            this.btLocalizar.Text = "Localizar";
+            this.btLocalizar.UseVisualStyleBackColor = true;
+            // 
+            // btAlterar
+            // 
+            this.btAlterar.Location = new System.Drawing.Point(215, 19);
+            this.btAlterar.Name = "btAlterar";
+            this.btAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btAlterar.TabIndex = 2;
+            this.btAlterar.Text = "Alterar";
+            this.btAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btExcluir
+            // 
+            this.btExcluir.Location = new System.Drawing.Point(309, 18);
+            this.btExcluir.Name = "btExcluir";
+            this.btExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btExcluir.TabIndex = 3;
+            this.btExcluir.Text = "Excluir";
+            this.btExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Location = new System.Drawing.Point(401, 19);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btSalvar.TabIndex = 4;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Location = new System.Drawing.Point(503, 18);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btCancelar.TabIndex = 5;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmModeloCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.pnBotoes);
+            this.Controls.Add(this.pnDados);
+            this.Name = "frmModeloCadastro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Modelo de Cadastro";
+            this.pnBotoes.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnDados;
+        private System.Windows.Forms.Panel pnBotoes;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.Button btExcluir;
+        private System.Windows.Forms.Button btAlterar;
+        private System.Windows.Forms.Button btLocalizar;
+        private System.Windows.Forms.Button btInserir;
+    }
+}
