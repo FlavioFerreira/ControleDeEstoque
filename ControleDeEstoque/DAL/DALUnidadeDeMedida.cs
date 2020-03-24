879b239594890e98188fe38ef953d7441102fbc5
Flávio Ferreira <flavinhoapf@hotmail.com>
2020-03-24 09:22:52 -0300

    Implementado a classe DALUnidadeDeMedida.

---
+++ ControleDeEstoque/DAL/DALUnidadeDeMedida.cs
@@ -0,0 +1,115 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALUnidadeDeMedida
+    {
+        private DALConexao conexao;
+
+        public DALUnidadeDeMedida(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        public void Incluir(ModeloUnidadeDeMedida modelo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conexao.ObjetoConexao;
+                cmd.CommandText = "insert into undmedida(umed_nome) values(@nome); select @@IDENTITY;";        
+                cmd.Parameters.AddWithValue("@nome", modelo.Umednome);
+                conexao.Conectar();
+                modelo.Umedcod = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        public void Alterar(ModeloUnidadeDeMedida modelo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conexao.ObjetoConexao;
+                cmd.CommandText = "update undmedida set umed_nome = @nome where umed_cod = @cod;";
+                cmd.Parameters.AddWithValue("@nome", modelo.Umednome);
+                cmd.Parameters.AddWithValue("@cod", modelo.Umedcod);
+                conexao.Conectar();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        public void Excluir(int codigo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conexao.ObjetoConexao;
+                cmd.CommandText = "delete from undmedida where umd_cod = @codigo;";
+                cmd.Parameters.AddWithValue("@codigo", codigo);
+                conexao.Conectar();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        public DataTable Localizar(String valor)
+        {
+            DataTable tabela = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("select * from undmedida where umed_nome like '%" + valor + "%'", conexao.StringConexao);
+            da.Fill(tabela);
+            return tabela;
+        }
+
+        public ModeloUnidadeDeMedida CarregaModeloUnidadeDeMedida(int codigo)
+        {
+            ModeloUnidadeDeMedida modelo = new ModeloUnidadeDeMedida();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "select * from undmedida where umed_cod = @codigo";
+            cmd.Parameters.AddWithValue("@codigo", codigo);
+            conexao.Conectar();
+            SqlDataReader registro = cmd.ExecuteReader();
+            if (registro.HasRows)
+            {
+                registro.Read();
+                modelo.Umedcod = Convert.ToInt32(registro["umed_cod"]);
+                modelo.Umednome = Convert.ToString(registro["umed_nome"]);
+                
+
+            }
+            conexao.Desconectar();
+            return modelo;
+        }
+
+    }
+}
