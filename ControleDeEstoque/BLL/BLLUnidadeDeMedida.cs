b7a01feae28a93f610160207b264d5ccc530a4e3
Flávio Ferreira <flavinhoapf@hotmail.com>
2020-03-24 09:50:43 -0300

    Implementado a classe BLLUnidadeDeMedida

---
+++ ControleDeEstoque/BLL/BLLUnidadeDeMedida.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+using DAL;
+using System.Data;
+
+namespace BLL
+{
+    public class BLLUnidadeDeMedida
+    {
+        private DALConexao conexao;
+
+        public BLLUnidadeDeMedida(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        public void Incluir(ModeloUnidadeDeMedida modelo)
+        {
+            if (modelo.Umednome.Trim().Length == 0)
+            {
+                throw new Exception("O nome da Unidade de Medida é Obrigatório!");
+
+            }
+            
+            modelo.Umednome = modelo.Umednome.ToUpper();
+
+            DALUnidadeDeMedida DAOObj = new DALUnidadeDeMedida(conexao);
+            DAOObj.Incluir(modelo);
+        }
+
+        public void Alterar(ModeloUnidadeDeMedida modelo)
+        {
+            if (modelo.Umednome.Trim().Length == 0)
+            {
+                throw new Exception("O nome da Unidade de Medida é Obrigatório!");
+
+            }
+            if (modelo.Umedcod <= 0)
+            {
+                throw new Exception("O código da Unidade de Medida é Obrigatório!");
+            }
+            
+            modelo.Umednome = modelo.Umednome.ToUpper();
+
+            DALUnidadeDeMedida DAOObj = new DALUnidadeDeMedida(conexao);
+            DAOObj.Alterar(modelo);
+        }
+
+        public void Excluir(int codigo)
+        {
+            DALUnidadeDeMedida DAOObj = new DALUnidadeDeMedida(conexao);
+            DAOObj.Excluir(codigo);
+        }
+
+        public DataTable Localizar(String valor)
+        {
+            DALUnidadeDeMedida DAOObj = new DALUnidadeDeMedida(conexao);
+            return DAOObj.Localizar(valor);
+        }
+
+        public ModeloUnidadeDeMedida CarregaModeloUnidadeDeMedida(int codigo)
+        {
+            DALUnidadeDeMedida DAOObj = new DALUnidadeDeMedida(conexao);
+            return DAOObj.CarregaModeloUnidadeDeMedida(codigo);
+        }
+    }
+}
