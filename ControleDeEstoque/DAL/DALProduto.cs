a9d92481d619521a08da89b463574791cb29846a
Flávio Ferreira <flavinhoapf@hotmail.com>
2020-03-29 21:14:02 -0300

    Implementado dentro da classe DALProduto, o método Inserir.

---
+++ ControleDeEstoque/DAL/DALProduto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace DAL
+{
+    public class DALProduto
+    {
+        private DALConexao conexao;
+
+        public DALProduto(DALConexao cx)
+        {
+            this.conexao = cx;
+        }
+
+        public void Inserir(ModeloProduto obj)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "insert into Produto (pro_nome, pro_descricao, pro_foto, pro_valorpago, pro_valorvenda, pro_qtde, undmed_cod, cat_cod, scat_cod)" +
+                " values (@nome, @descricao, @foto, @valorpago, @valorvenda, @qtde, @undmedcod, @catcod, @scatcod); select @@IDENTITY; ";
+            cmd.Parameters.AddWithValue("@nome", obj.ProNome);
+            cmd.Parameters.AddWithValue("@descricao", obj.ProDescricao);
+            cmd.Parameters.Add("@foto", System.Data.SqlDbType.Image);
+            if(obj.ProFoto == null)
+            {
+                cmd.Parameters["@foto"].Value = DBNull.Value;
+            }
+            else
+            {
+                cmd.Parameters["@foto"].Value = obj.ProFoto;
+            }
+            cmd.Parameters.AddWithValue("@valorpago", obj.ProValorpago);
+            cmd.Parameters.AddWithValue("@valorvenda", obj.ProValorvenda);
+            cmd.Parameters.AddWithValue("@qtde", obj.ProQtde);
+            cmd.Parameters.AddWithValue("@undmedcod", obj.UmedCod);
+            cmd.Parameters.AddWithValue("@catcod", obj.CatCod);
+            cmd.Parameters.AddWithValue("@scatcod", obj.ScatCod);
+
+            conexao.Conectar();
+            obj.ProCod = Convert.ToInt32(cmd.ExecuteScalar());
+            conexao.Desconectar();
+
+        }
+
+    }
+}
